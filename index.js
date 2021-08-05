fa40479ba792175a656fdaf62c2f94faa7359d24
Inderjeet <jeetinder670@gmail.com>
2021-08-05 21:41:57 +0530

    javascript for adding items to the todos list and some CSS tweaks. Next is to add delete item feature and clear list feature

--- index.js
+++ index.js
@@ -1,16 +1,93 @@
 add = document.querySelector(".submit_button");
 tit = document.querySelector(".input_div_title_input");
-desc = document.querySelector(".input_div_description_input")
+desc = document.querySelector(".input_div_description_input");
+insert = document.querySelector('.table_div_body');
 
 title = ''
 description = ''
 
 
 
-add.addEventListener("click",update())
+add.addEventListener("click",update)
+
+
+
+
 
 
 function update(){
 
+    if(localStorage.getItem('todolist')== null){
+        if(tit.value != null && desc.value != null){
+
+            array = [];
+        array.push([tit.value,desc.value])
+        localStorage.setItem('todolist', JSON.stringify(array) )
+
+
+
+        }
+        
+    }
+    else{
+        if(tit.value != null && desc.value != null){
+            str = localStorage.getItem('todolist');
+        array = JSON.parse(str)
+        array.push([tit.value,desc.value])
+        localStorage.setItem('todolist', JSON.stringify(array))
+        }
+    }
+    str = localStorage.getItem('todolist')
+    array = JSON.parse(str)
+
+    strs = " "
+
+    array.forEach((element,index )=> {
+       
+
+        strs += `<tr>
+        <td>${index+1}</td>
+        <td>${element[0]}</td>
+        <td>${element[1]}</td>
+        <td><button  class="table_div_table_head_row_data_button" ><h3 style="color:white; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif">Delete</h3></button></td></td>
+    </tr>`
+
+    
+    });
+
+
+    insert.innerHTML = strs
+
+    
+
 
 }
+
+function runbegin(){
+
+    str = localStorage.getItem('todolist')
+    array = JSON.parse(str)
+
+    strs = " "
+
+    array.forEach((element,index )=> {
+       
+
+        if(localStorage.getItem('todolist') != null){
+            strs += `<tr>
+        <td>${index+1}</td>
+        <td>${element[0]}</td>
+        <td>${element[1]}</td>
+        <td><button  class="table_div_table_head_row_data_button" ><h3 style="color:white; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif">Delete</h3></button></td></td>
+
+    </tr>`
+
+        }
+    
+    });
+
+    insert.innerHTML = strs
+
+}
+
+runbegin()

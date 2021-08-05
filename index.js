cd5b25456d7d1940e8d6ce8bfe529d1c628bb44b
Inderjeet <jeetinder670@gmail.com>
2021-08-05 20:02:13 +0530

    commit 2: Finish HTML and CSS

---
+++ index.js
@@ -0,0 +1,16 @@
+add = document.querySelector(".submit_button");
+tit = document.querySelector(".input_div_title_input");
+desc = document.querySelector(".input_div_description_input")
+
+title = ''
+description = ''
+
+
+
+add.addEventListener("click",update())
+
+
+function update(){
+
+
+}

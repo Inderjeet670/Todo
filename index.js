1d7ed16879b6e7b07cd43bf069b3c7d86ba98a43
Inderjeet <jeetinder670@gmail.com>
2021-08-06 12:01:34 +0530

    responsive design CSS and inputs empty after adding to the list

--- index.js
+++ index.js
@@ -26,8 +26,6 @@ function update(){
         array.push([tit.value,desc.value])
         localStorage.setItem('todolist', JSON.stringify(array) )
 
-
-
         }
         
     }
@@ -39,26 +37,9 @@ function update(){
         localStorage.setItem('todolist', JSON.stringify(array))
         }
     }
-    str = localStorage.getItem('todolist')
-    array = JSON.parse(str)
-
-    strs = " "
-
-    array.forEach((element,index )=> {
-       
-
-        strs += `<tr>
-        <td>${index+1}</td>
-        <td>${element[0]}</td>
-        <td>${element[1]}</td>
-        <td><button onclick = "deleted(${index})"  class="table_div_table_head_row_data_button" ><h3 style="color:white; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif">Delete</h3></button></td></td>
-    </tr>`
-
-    
-    });
-
-
-    insert.innerHTML = strs
+    tit.value = ''
+    desc.value = ''
+    runbegin()
 
     
 
@@ -66,9 +47,23 @@ function update(){
 }
 
 function runbegin(){
+    
+    array = []
 
-    str = localStorage.getItem('todolist')
-    array = JSON.parse(str)
+    if(localStorage.getItem('todolist')==null){
+        array = []
+        localStorage.setItem('todolist', JSON.stringify(array))
+
+    }
+    else{
+
+        str = localStorage.getItem('todolist')
+        array = JSON.parse(str)
+
+
+    }
+
+  
 
     strs = " "
 
@@ -84,16 +79,6 @@ function runbegin(){
 
     </tr>`
 
-        }
-        else{
-        
-            strs += `<tr>
-        <td>${index+1}</td>
-        <td>${element[0]}</td>
-        <td>${element[1]}</td>
-        <td><button onclick = "deleted(${index})" class="table_div_table_head_row_data_button" ><h3 style="color:white; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif">Delete</h3></button></td></td>
-
-    </tr>`
         }
     
     });
